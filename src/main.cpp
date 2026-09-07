f2ddf1dde562881b0f14250106f2e3618a12a892
Aleksandr <nvzarov.as@gmail.com>
2026-09-07 15:08:12 +0300

    base template

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#include <raylib.h>
+
+int main(void)
+{
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "Я СОСУ ЧЛЕН");
+
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+
+            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+        EndDrawing();
+        
+    }
+
+    
+    CloseWindow();        // Close window and OpenGL context
+    
+
+    return 0;
+}
