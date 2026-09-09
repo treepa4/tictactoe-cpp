ad4dfb0cfe6f3f7c91e5f056911ed8bb527398a3
Aleksandr <nvzarov.as@gmail.com>
2026-09-09 15:08:09 +0300

    Вынес функции хода в отдельные функции, заменил ismousebuttondown на ismousebuttonpressed

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,38 @@ void DrawGrid(int screensize)
         DrawLine(225+(i*150), 75, 225+(i*150), screensize-75, BEIGE);
         DrawLine(75, 225+(i*150), screensize-75, 225+(i*150), BEIGE);
     }
+}
+enum Cell {EMPTY, X, O};
+Cell board[3][3] = {EMPTY};
+bool is_player_turn = true;
 
+
+void PlayerInput(int screenSize) {
+    if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+                Vector2 mouse = GetMousePosition();
+                if (mouse.x >= 75 && mouse.x <= screenSize - 75 && mouse.y >= 75 && mouse.y <= screenSize - 75) {
+                    int col = ((int)mouse.x - 75) / 150;
+                    int row = ((int)mouse.y - 75) / 150;
+                    if (board[row][col] == EMPTY && is_player_turn) {
+                        board[row][col] = X;
+                        is_player_turn = false;
+                    }
+                }
+            }
+}
+
+void AiMove(int screenSize) {
+    if (!is_player_turn) {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                if (board[row][col] == EMPTY) {
+                    board[row][col] = O;
+                    is_player_turn = true;
+                    return;
+                }
+            }
+        }
+    }
 }
 
 
@@ -23,13 +54,14 @@ int main(void)
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
-
+        PlayerInput(screenSize);
+        AiMove(screenSize);
         BeginDrawing();
 
             ClearBackground(WHITE);
             DrawGrid(screenSize);
             // DrawText("penis!", 190, 200, 20, BEIGE);
-
+            
         EndDrawing();
         
     }
